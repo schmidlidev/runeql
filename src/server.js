f1ab2deb7c896881be78519a2466249740c65e90
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-06 19:16:31 -0700

    mongo connection and resolvers

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { loadSchema } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import mongodb from "mongodb";
+
+const mongo = await mongodb.MongoClient.connect("mongodb://localhost:27017/");
+const Items = await mongo.db("osrs").collection("items");
 
 // Construct a schema, using GraphQL schema language
 const schema = await loadSchema("./src/schema/schema.gql", {
@@ -10,8 +14,19 @@ const schema = await loadSchema("./src/schema/schema.gql", {
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return "Hello world!";
+  items: async () => {
+    return await Items.find({}).toArray();
+  },
+  item: async ({ id }) => {
+    return await Items.findOne({ id: id });
+  },
+  startsWith: async ({ prefix }) => {
+    return await Items.find({
+      name: RegExp(prefix, "i"),
+      duplicate: false,
+    })
+      .limit(10)
+      .toArray();
   },
 };
 
