96e4c06289314d9eba738e75ed8154ceb879dba1
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-06 22:10:31 -0700

    default filter duplicate items

--- src/resolvers/items.js
+++ src/resolvers/items.js
@@ -1,16 +1,21 @@
 import mongo from "../mongo.js";
 
-export default async function ({ nameLike, limit }) {
+export default async function ({ duplicate, nameLike, limit }) {
   const Items = mongo.collection("items");
 
-  let query = {};
+  // Required and default fields
+  let query = {
+    duplicate: duplicate,
+  };
 
+  // Optional query parameters
   if (nameLike) {
     query.name = RegExp(nameLike, "i");
   }
 
   let result = Items.find(query);
 
+  // Optional filtering parameters
   if (limit) {
     result = result.limit(limit);
   }
