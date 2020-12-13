da2fa9a60566a1b4429e60abf34965ae1f16191e
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-12 20:55:34 -0700

    add more params for items searches

--- src/resolvers/items.js
+++ src/resolvers/items.js
@@ -1,6 +1,6 @@
 import mongo from "../mongo.js";
 
-export default async ({ nameLike, limit }) => {
+export default async ({ nameLike, qualifiedNameLike, slot, limit }) => {
   const Items = mongo.collection("items");
 
   // Required and default fields
@@ -9,6 +9,12 @@ export default async ({ nameLike, limit }) => {
   // Optional query parameters
   if (nameLike) {
     query.name = RegExp(nameLike, "i");
+  }
+  if (qualifiedNameLike) {
+    query.qualified_name = RegExp(qualifiedNameLike, "i");
+  }
+  if (slot) {
+    query["equipment.slot"] = slot;
   }
 
   let result = Items.find(query);
