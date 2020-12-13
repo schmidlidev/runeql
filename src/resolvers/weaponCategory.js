b0e85b40375d96cc2769091e27fc0e01693b3805
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-13 10:29:45 -0700

    remove log

--- src/resolvers/weaponCategory.js
+++ src/resolvers/weaponCategory.js
@@ -1,7 +1,6 @@
 import mongo from "../mongo.js";
 
 export default async ({ name }) => {
-  console.log(name);
   const WeaponCategories = mongo.collection("weaponCategories");
 
   return await WeaponCategories.findOne({ name: name });
