8b3f3201760be5d038d7ce92ccb228afd3a190ea
Austin Schmidli <austin.schmidli@gmail.com>
2020-12-22 12:26:35 -0700

    remove test code

--- src/resolvers/item.js
+++ src/resolvers/item.js
@@ -3,7 +3,5 @@ import mongo from "../mongo.js";
 export default async ({ id }) => {
   const Items = mongo.collection("items");
 
-  let item = await Items.findOne({ id: id });
-  if (item === null) console.log(id);
-  return item;
+  return await Items.findOne({ id: id });
 };
